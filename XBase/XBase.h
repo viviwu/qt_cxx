104d063fd243fcd782571fcb1be7f5256acb4e8e
viviwu <vivi705@qq.com>
2020-03-18 12:17:50 +0800

    .
    .

--- XBase/XBase.h
+++ XBase/XBase.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char XBaseVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XBase/PublicHeader.h>
 
-#import <XBase/NSObject+XSObject.h>
+#import <XBase/XObject.h>
